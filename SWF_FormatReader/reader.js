4e752c5605cc598e0422f7f923f56dba67dcaed2
HunorAbraham <hunor335@gmail.com>
2025-02-27 10:09:07 +0100

    reconsideration

--- SWF_FormatReader/reader.js
+++ SWF_FormatReader/reader.js
@@ -4,19 +4,4 @@ class Tag{
     this.length = length;
   }
 }
-class Character(){
-  constructor(ID, type, content){
-    this.ID = ID;
-    this.type = type;
-    this.content = content;
-    this.depth_value = 0; //set later? perhaps redundant
-  }
-}
-class Reader{
-  constructor(){
-    this.dictionary = {};
-    this.display_list = [];
-  }
-  
-}
 //STOP, don't implement yet >:(
