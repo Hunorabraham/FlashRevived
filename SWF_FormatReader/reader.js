6ad610f3b9d4be231465a0169268be79bfc74671
HunorAbraham <hunor335@gmail.com>
2025-02-27 10:15:21 +0100

    testing related stuff 2

--- SWF_FormatReader/reader.js
+++ SWF_FormatReader/reader.js
@@ -4,8 +4,13 @@ class Tag{
     this.length = length;
   }
 }
-function hello(){console.log("hello, testing complete..."); console.error("THERE ARE ALWAYS ERRORS");}
-function read(file){
-
+/**
+ * I don't want to bother with modules and stuff, confession: I have no clue how to make them work, I tried and failed
+*/
+class READER{
+  static hello(){console.log("hello, testing complete..."); console.error("THERE ARE ALWAYS ERRORS");}
+  static read(file){
+  
+  }
 }
 //STOP, don't implement yet >:(
