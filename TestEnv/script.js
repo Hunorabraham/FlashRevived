a8d417e61916198bc62ca92ea9eade6cfdecdfbc
HunorAbraham <hunor335@gmail.com>
2025-02-27 10:26:25 +0100

    testing file input

--- TestEnv/script.js
+++ TestEnv/script.js
@@ -1,1 +1,13 @@
-READER.hello();
+let file_in = document.querySelector("input");
+file_in.onchange = (e)=>{
+    let file = e.target.files[0];
+    console.log(file);
+    if(!file){
+        console.error("What? There is no file?");
+        return;
+    }
+    if(file.type != "application/x-shockwave-flash"){
+        console.error("WOHA there pal, that is not a shockwafe flash file!");
+        return;
+    }
+}
