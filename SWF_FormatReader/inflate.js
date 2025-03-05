1f3b02d323d4e5701f731ea8c584f00a274b2dee
HunorAbraham <hunor335@gmail.com>
2025-03-05 13:29:18 +0100

    I've been betrayed

---
+++ SWF_FormatReader/inflate.js
@@ -0,0 +1,22 @@
+/**
+*inflate utilities
+*perhaps unnecessary
+*/
+class INFUTIL{
+  
+}
+
+const Z_STREAM_STATUS = Object.freeze({
+  
+});
+
+class ZSTATE{
+  constructor(){
+    this.status = 0;
+  }
+}
+
+
+function inflate(r_stream){
+  
+}
