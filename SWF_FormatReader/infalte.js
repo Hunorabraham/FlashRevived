80f6e9aaa051891e9ebffa552f1ce20e700f417a
Ábrahám Hunor <102142802+Hunorabraham@users.noreply.github.com>
2025-03-05 15:16:42 +0100

    header check

--- SWF_FormatReader/infalte.js
+++ SWF_FormatReader/infalte.js
@@ -1,39 +1,45 @@
-
-
 class ZLIB_DECODER{
-  constructor(inflation_target){
+  constructor(inflation_target, size_hint){
     this.data = new Uint8Array(inflation_target);
     this.mode = "HEAD";
     this.bits = 0;
     this.hold = 0;
     this.remaining = this.data.length;
     this.reading_position = 0;
+    this.error_message = "";
+    this.result = new Uint8Array(new ArrayBuffer(size_hint));
   }
-  getInflated(){
+  inflate(){
     while(true){
     switch(this.mode){
     case "HEAD":
       this.GETBITS(16);
       //slight bit magic for checksum, "!== 0" is needed to convert number to bool correctly (c-style)
-      if((this.BITS(8) << 8) + (this.hold >> 8) % 31 !== 0){
-        throw("incorrect header check");
+      if(((this.BITS(8) << 8) + (this.hold >> 8)) % 31 !== 0){
+        this.error_message = "incorrect header check";
+        return "ERR";
       }
+      this.mode = "TESTNEXT";
       break;
     default:
-      console.error("Not implemented yet");
-      return this;
+      this.error_messge = "Not implemented yet/unexpected mode: " + this.mode;
+      return "ERR";
     }//switch
     }//while
   }
-  private BITS(n) {return this.hold & ((1 << n) - 1);}
-  private GETBITS(n) {
+  BITS(n) {return this.hold & ((1 << n) - 1);}
+  GETBITS(n) {
     while(this.bits < n){
-      this.GETBYTE();
+      if(this.GETBYTE() !== "OK"){
+        return "ERR";
+      }
     }
+    return "OK";
   }
-  private GETBYTE(){
+  GETBYTE(){
     if(this.remaining <= 0){
-      throw("data eneded unexpectedly");
+      this.error_message = "data ended unexpectedly";
+      return "ERR";
     }
     this.remaining--;
     this.hold += this.data[this.reading_position++] << this.bits;
@@ -41,3 +47,4 @@ class ZLIB_DECODER{
     return "OK";
   }
 }
+
