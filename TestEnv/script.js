a47a0bb44a31bcbacb775cfe7f94fb396a1eb912
HunorAbraham <hunor335@gmail.com>
2025-03-09 14:42:08 +0100

    I forgot

--- TestEnv/script.js
+++ TestEnv/script.js
@@ -1,13 +1,17 @@
 let file_in = document.querySelector("input");
 file_in.onchange = (e)=>{
-    let file = e.target.files[0];
-    console.log(file);
-    if(!file){
-        console.error("What? There is no file?");
-        return;
-    }
-    if(file.type != "application/x-shockwave-flash"){
-        console.error("WOHA there pal, that is not a shockwafe flash file!");
-        return;
-    }
+  let file = e.target.files[0];
+  if(!file){
+      console.error("What? There is no file?");
+      return;
+  }
+  if(file.type != "application/x-shockwave-flash"){
+      console.error("WOHA there pal, that is not a shockwafe flash file!");
+      return;
+  }
+  READER.read(file).then((data)=>{
+      console.log(data);
+  }).catch((err)=>{
+      console.error(err);
+  });
 }
