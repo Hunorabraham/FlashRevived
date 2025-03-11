7d2d09113ee38b49e40c9681e7d6debb42df705b
Ábrahám Hunor <102142802+Hunorabraham@users.noreply.github.com>
2025-03-11 13:45:33 +0100

    restructure

--- SWF_FormatReader/inflate.js
+++ SWF_FormatReader/inflate.js
@@ -1,95 +1,109 @@
 class ZLIB_DECODER{
   constructor(inflation_target, size_hint){
     this.data = new Uint8Array(inflation_target);
-    this.mode = "HEAD";
     this.bits = 0;
     this.hold = 0;
-    this.remaining = this.data.length;
     this.reading_position = 0;
     this.error_message = "";
     this.result = new Uint8Array(new ArrayBuffer(size_hint));
     this.last = false;
     this.window_width = 0;
+    this.written = 0; // number of bytes written to result
+    this.tree = {};
   }
   inflate(){
-    while(true){
-    switch(this.mode){
-    case "HEAD":
       this.GETBITS(16);
       //slight bit magic for checksum, "!== 0" is needed to convert number to bool correctly (c-style)
       if(((this.BITS(8) << 8) + (this.hold >> 8)) % 31 !== 0){
         this.error_message = "incorrect header check";
-        return "ERR";
+        return false;
       }
       if(this.BITS(4) !== 8){//deflate encoding
         this.error_message = "unknown compression method";
-        return "ERR";
+        return false;
       }
       this.CONSUMEBITS(4);
       this.window_width = this.BITS(4)+8;
       if(this.window_width > 15){
         this.error_message = "invalid window size: " + this.window_width;
-        return "ERR";
+        return false;
       }
-      this.mode = (this.hold & 0x200) !== 0 ? "DICTID" : "TYPE";
+      if((this.hold & 0x200) !== 0){
+        this.error_message = "use of external dict is not supported";
+        return false;
+      }
       this.CLEARACCUMULATOR();
       console.log(`Header read\n\tmethod: DEFLATE\n\twindow width: ${this.window_width}`);
-      break;//HEAD
-    case "DICTID":
-      this.error_message = "use of external dict is not supported"
-      return "ERR";
-    case "TYPE":
-      if(this.last){
-        this.ALINGTOBYTE();
-        this.mode = "CHECK";
-        break;
-      }
-      this.GETBITS(3);
-      this.last = this.BITS(1);
-      this.CONSUMEBITS(1);
-      //this might need a bit more elbow grease
-      switch(this.BITS(2)){
-        //stored
-        case 0:
-          this.mode = "STORED";
+      let block_part = "head";
+      while(true){
+      switch(block_part){
+        case "head":
+          if(this.last){
+            this.ALIGNTOBYTE();
+            this.Check();
+          }
+          this.GETBITS(3);
+          if(this.BITS(1)===1) this.last = true;
+          this.CONSUMEBITS(1);
+          switch(this.BITS(2)){
+            case 0:
+              block_part = "non-compressed";
+              this.ALIGNTOBYTE();
+              this.CLEARACCUMULATOR();
+              break;
+            case 1:
+              block_part = "LEN";
+              break;
+            case 2:
+              block_part = "dynamic-huffman";
+              break;
+            case 3:
+              this.error_message = "invalid block type";
+              return false;
+          }
           break;
-        case 1:
-          this.fixedTables();
-          this.mode = "LEN";
+        case "non-compressed":
+          this.GETBITS(16);
+          let lenght = this.littleEndian16(this.hold);
+          this.CLEARACCUMULATOR();
+          //I don't care about the one complement
+          this.GETBITS(16);
+          this.CLEARACCUMULATOR();
+          //write literal bytes
+          this.writeChunk(this.data.slice(this.reading_position, length));
+          //jump over the written bytes
+          this.reading_position += lenght;
+          //read next header
+          block_part = "head";
           break;
-        case 2:
-          this.mode = "TABLE";
+        case "dynamic-huffman":
+          
+          
+          block_part = "intentional error";
           break;
-        case 3:
-          this.error_message = "invalid block type";
-          return "ERR";
-      }
-      this.CONSUMEBITS(2);
-      break;
-    default:
-      this.error_message = "Not implemented yet/unexpected mode: " + this.mode;
-      return "ERR";
-    }//switch
-    }//while
+        default:
+          this.error_message = "unexpected/not implemented block part: " + block_part;
+          return false;
+      }//Switch
+      }//while
   }
   BITS(n) {return this.hold & ((1 << n) - 1);}
   GETBITS(n) {
     while(this.bits < n){
-      if(this.GETBYTE() !== "OK"){
-        return "ERR";
+      if(!this.GETBYTE()){
+        return false;
       }
     }
-    return "OK";
+    return true;
   }
   GETBYTE(){
-    if(this.remaining <= 0){
+    if(this.reading_position >= this.data.length){
       this.error_message = "data ended unexpectedly";
-      return "ERR";
+      return false;
     }
-    this.remaining--;
     this.hold += this.data[this.reading_position++] << this.bits;
     this.bits += 8;
-    return "OK";
+    return true;
   }
   CONSUMEBITS(n){
     this.hold >>= n;
@@ -102,5 +116,16 @@ class ZLIB_DECODER{
   CLEARACCUMULATOR(){
     this.hold = 0;
     this.bits = 0;
+  }
+  littleEndian16(bytes){
+    return ((bytes & 0xff) << 8) + ((bytes & 0xff00) >> 8);
+  }
+  write(one_byte){
+    this.result[this.written++] = one_byte;
+  }
+  writeChunk(array){
+    for(one_byte of array){
+      this.result[this.written++] = one_byte;
+    }
   }
 }
