7bd6604602fdf24c2c3cd0e389d3f5e64db3f14e
Ábrahám Hunor <102142802+Hunorabraham@users.noreply.github.com>
2025-03-12 14:26:17 +0100

    Update inflate.js

--- SWF_FormatReader/inflate.js
+++ SWF_FormatReader/inflate.js
@@ -1,3 +1,6 @@
+/*
+* See compliance at  Request for Comments: 1950-1951
+*/
 class ZLIB_DECODER{
   constructor(inflation_target, size_hint){
     this.data = new Uint8Array(inflation_target);
@@ -10,6 +13,63 @@ class ZLIB_DECODER{
     this.window_width = 0;
     this.written = 0; // number of bytes written to result
     this.current_tree = {};
+  }
+  static MAXBITS = 16;
+  static createStringCode(code, len){
+    let result = "";
+    for(let i = 0n; i < len; i++){
+      result = (((code >> i) & 1n) == 0n ? "0" : "1") + result;
+    }
+    return result;
+  }
+  static generateTree(lengths){
+    //get count of lengths
+    let bl_count = Array(ZLIB_DECODER.MAXBITS).fill(0n);
+    lengths.forEach(len=>bl_count[len]++);
+    //get smallest code values
+    let code = 0n;
+    let next_code = Array(ZLIB_DECODER.MAXBITS).fill(0n);
+    for(let bits = 1n; bits <= ZLIB_DECODER.MAXBITS; bits++){
+      code = (code + bl_count[bits-1n]) << 1n;
+      next_code[bits] = code;
+    }
+    let flat_tree = [];
+    for(let n = 0; n < lengths.length; n++){
+      let len = lengths[n];
+      if(len == 0) continue;
+      flat_tree[n] = ZLIB_DECODER.createStringCode(next_code[len],len);
+      next_code[len]++;
+    }
+    let tree = [{partial_code: ""}];
+    let current_code = "";
+    let current_place = 0;
+    let placed_codes = 0;
+    let total_codes = lengths.filter(len => len != 0).length;
+    let iter = 0;
+    let maxiter = 2000;
+    while(placed_codes < total_codes){
+      current_code = tree[current_place].partial_code;
+      let match = flat_tree.find(code=>code===current_code);
+      if(match == undefined){
+        //no match
+        //change current node to branch
+        tree[current_place] = {code: false, left: null, right: null}; //partial code for debugging
+        //push two children onto the tree
+        tree[current_place].left = tree.length;
+        tree.push({code: false, partial_code: current_code+"0"});
+        tree[current_place].right = tree.length;
+        tree.push({code: false, partial_code: current_code+"1"});
+      }
+      else{
+        //change current node to code
+        tree[current_place] = {code: true, value: flat_tree.indexOf(match)}; //partial code for debugging
+        placed_codes++;
+      }
+      //step forward
+      current_place++;
+      if(iter++ >= maxiter) break;
+    }
+    return tree;
   }
   inflate(){
       this.GETBITS(16);
@@ -64,7 +124,7 @@ class ZLIB_DECODER{
           break;
         case "non-compressed":
           this.GETBITS(16);
-          let lenght = this.littleEndian16(this.hold);
+          let length = this.littleEndian16(this.hold);
           this.CLEARACCUMULATOR();
           //I don't care about the one complement
           this.GETBITS(16);
@@ -72,39 +132,79 @@ class ZLIB_DECODER{
           //write literal bytes
           this.writeChunk(this.data.slice(this.reading_position, length));
           //jump over the written bytes
-          this.reading_position += lenght;
+          this.reading_position += length;
           //read next header
           block_part = "head";
           break;
         case "dynamic-huffman":
           //table info
-          this.GETBITS(14);
+          if(!this.GETBITS(14)) return false;
           let lit_length_count = this.BITS(5)+257;
           this.CONSUMEBITS(5);
           let distance_count = this.BITS(5)+1;
           this.CONSUMEBITS(5);
-          let code_lenght_code_count = this.BITS(4)+4;
+          let code_length_code_count = this.BITS(4)+4;
           this.CONSUMEBITS(4);
+          
           //get lengths of length codes
-          let lenght_lengths = Array(19).fill(0);
-          console.log(lenght_lengths);
-          //a little more magic
+          let length_lengths = Array(19).fill(0);
+          //a little index magic for specific order
           let indicies = [16, 17, 18, 0, 8, 7, 9, 6, 10, 5, 11, 4, 12, 3, 13, 2, 14, 1, 15];
-          //we already hace 2 bits of the first
-          this.GETBITS(1);
-          lenght_lengths[indicies[0]] = BITS(3);
-          this.CONSUMEBITS(3);
-          for(let i = 1; i < code_lenght_code_count; i++){
+          for(let i = 0; i < code_length_code_count; i++){
             this.GETBITS(3);
-            lenght_lengths[i] = this.BITS(3);
+            length_lengths[indicies[i]] = this.BITS(3);
             this.CONSUMEBITS(3);
           }
+          console.log(code_length_code_count, length_lengths);
           
           //generate length tree
-          let length_tree = {};
-          
-          
-          
+          let length_tree = ZLIB_DECODER.generateTree(length_lengths);
+          //get lenths of literal/length alphabet
+          let literal_lengths = []; //intentionally only named it literal
+          let current_index = 0;
+          for(let i = 0; i < lit_length_count; i++){
+            //get next bit
+            this.GETBITS(1);
+            let step = this.BITS(1) == 0 ? "left" : "right";
+            this.CONSUMEBITS(1);
+            
+            //move down in tree
+            current_index = length_tree[current_index][step];
+            //check if reached valid symbol
+            if(!length_tree[current_index].code) continue;
+            //do alphabet with value
+            let symbol = length_tree[current_index].value;
+            if(symbol<16){
+              literal_lengths.push(symbol);
+            }
+            else if(symbol == 16){
+              this.GETBITS(2);
+              let repeat_count = this.BITS(2)+3;
+              this.CONSUMEBITS(2);
+              for(let i = 0; i < repeat_count; i++){
+                literal_lengths.push(literal_lengths[literal_lengths.length-1]);
+              }
+            }
+            else if(symbol == 17){
+              this.GETBITS(3);
+              let zeroes = this.BITS(3)+3;
+              this.CONSUMEBITS(3);
+              for(let i = 0; i < zeroes; i++){
+                literal_lengths.push(0);
+              }
+            }
+            else{
+              this.GETBITS(7);
+              let zeroes = this.BITS(7)+11;
+              this.CONSUMEBITS(7);
+              for(let i = 0; i < zeroes; i++){
+                literal_lengths.push(0);
+              }
+            }
+            //go back to head of tree
+            current_index = 0;
+          }//literal lengths for
+          console.log(literal_lengths);
           block_part = "intentional error";
           break;
         default:
@@ -117,6 +217,7 @@ class ZLIB_DECODER{
   GETBITS(n) {
     while(this.bits < n){
       if(!this.GETBYTE()){
+        this.error_message += "; asking for: " + n + "bits";
         return false;
       }
     }
@@ -125,6 +226,10 @@ class ZLIB_DECODER{
   GETBYTE(){
     if(this.reading_position >= this.data.length){
       this.error_message = "data ended unexpectedly";
+      return false;
+    }
+    if(this.bits + 8 > 32){
+      this.error_message = "hold size exhausted, too many bits requested, current: " + this.bits;
       return false;
     }
     this.hold += this.data[this.reading_position++] << this.bits;
