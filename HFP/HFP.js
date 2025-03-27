79516a075e7c77219004d5d44859bf098ce76ad4
HunorAbraham <hunor335@gmail.com>
2025-03-27 21:32:49 +0100

    swf header reading complete, some record reads also

--- HFP/HFP.js
+++ HFP/HFP.js
@@ -28,13 +28,17 @@ class FLASH_PLAYER{
     console.log("starting execution, SWF Version: " + this.data[3]);
     this.reading_position = 8n;
     //read in rest of swf header
-    
-    this.TYPE_SIGNATURES["UI8"]();
-    console.log(this.nextTag());
+    this.frame_size = this.getRecord("RECT");
+    this.frame_rate = this.getRecord("FIXED8");
+    this.frame_count = this.getRecord("UI16");
+    this.renderer.initialise(Number(this.frame_size.width), Number(this.frame_size.height));
+    this.test();
+    console.log(this);
   }
   test(){
     this.renderer.draw_general_debug("fillRect", 10,10,100,100);
   }
+  byteAlign(){this.hold = 0n; this.bits_remaining = 0n;}
   nextTag(){
     //header
     let tag = {};
@@ -47,28 +51,47 @@ class FLASH_PLAYER{
     if(tag.length === 0x3fn) tag.length = this.getBytes(4n);
     return tag;
   }
-  nextByte(){return this.data[this.reading_position++];}
-  //can be made better
+  //byte aligned
+  nextByte(){
+    this.byteAlign();
+    return this.data[this.reading_position++];
+  }
+  getHeldBits(n){
+    if(this.bits_remaining < n) throw("requested too many bits: " + n);
+    this.bits_remaining -= n;
+    return (this.hold >> (this.bits_remaining)) & ((1n << n) - 1n);
+  }
+  advanceHold(){
+    this.hold = BigInt(this.nextByte());
+    this.bits_remaining = 8n;
+  }
   getBits(n){
     let num = 0n;
     let consumed_bits = 0n;
-    while(consumed_bits < n){
-      if(this.bits_remaining == 0){
-        this.hold = BigInt(this.nextByte());
-        this.bits_remaining = 8;
-      }
-      let needed_bits = Math.Min(n-consumed_bits, this.bits_remaining);
-      num += (this.hold >> (this.bits_remaining - needed_bits)) << consumed_bits;
-      this.bits_remaining -= needed_bits;
-      consumed_bits += needed_bits;
+    if(this.bits_remaining >= n){
+      num = this.getHeldBits(n);
+      return num;
+    }
+    consumed_bits = this.bits_remaining;
+    num = this.getHeldBits(this.bits_remaining);
+    while((n - consumed_bits) >= 8n){
+      num <<= 8n;
+      num += BigInt(this.nextByte());
+      consumed_bits += 8n;
+    }
+    //for safety
+    if(consumed_bits > n){ throw("somehow read too many bits"); }
+    if(consumed_bits < n){
+      this.advanceHold();
+      num <<= n-consumed_bits;
+      num += this.getHeldBits(n-consumed_bits);
     }
     return num;
   }
   //only call this for byte aligned, little endian values
   getBytes(n){
     //hold and bits remaning are reset
-    this.hold = 0n;
-    this.bits_remaining = 0n;
+    this.byteAlign();
     let num = 0n;
     for(let i = 0n; i < n; i++){
       num += BigInt(this.nextByte()) << (i*8n);
@@ -85,19 +108,43 @@ class FLASH_PLAYER{
     "SI16" : ()=>{return this.getBytes(2);},
     "SI32" : ()=>{return this.getBytes(4);},
     "SI64" : ()=>{return this.getBytes(8);},
+    "FIXED8": ()=>{return this.nextByte()/256 + this.nextByte();},
     "RECT": ()=>{
+      this.byteAlign();
       
+      let len = this.getBits(5n);
+      let Xmin = this.getBits(len);
+      let Xmax = this.getBits(len);
+      let Ymin = this.getBits(len);
+      let Ymax = this.getBits(len);
+      return {
+        "Xmin": Xmin, 
+        "Xmax": Xmax, 
+        "Ymin": Ymin, 
+        "Ymax": Ymax,
+        //JS like properties
+        "left": Xmin,
+        "right": Ymin,
+        "width": (Xmax-Xmin)/20n,
+        "height": (Ymax-Ymin)/20n,
+      };
     }
-  }
+  };
   //recursive function to parse record data into a JS object
   getRecord(type){
-    return this.TYPE_SIGNATURES["type"]();
+    return this.TYPE_SIGNATURES[type]();
   }
 }
 class HTML_CANVAS_RENDERER{
-  constructor(target){
-    this.can = target;
-    this.ctx = target.getContext("2d");
+  constructor(place){
+    this.place = place;
+  }
+  initialise(width, height){
+    this.target = document.createElement("canvas");
+    this.target.width = width;
+    this.target.height = height;
+    this.place.appendChild(this.target);
+    this.ctx = this.target.getContext("2d");
   }
   /*
   *for debugging only please don't use
